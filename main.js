d718738634ec606040ee327790d67ecc0d30ac47
AlessandroCingolani <alessandro.cingolani93@gmail.com>
2023-09-25 13:59:29 +0200

    [add]:data provided for create post

--- main.js
+++ main.js
@@ -59,39 +59,40 @@ const posts = [
 const cardPost = document.querySelector('.post');
 
 
-// cardPost.innerHTML = '';
+cardPost.innerHTML = '';
 
 posts.forEach((post) => {
     cardPost.innerHTML += `<div class="post">
     <div class="post__header">
         <div class="post-meta">                    
             <div class="post-meta__icon">
-                <img class="profile-pic" src="https://unsplash.it/300/300?image=15" alt="Phil Mangione">                    
+                <img class="profile-pic" src="${post.author.image}  alt="${post.author.name}">                    
             </div>
             <div class="post-meta__data">
-                <div class="post-meta__author">Phil Mangione</div>
+                <div class="post-meta__author">${post.author.name}</div>
                 <div class="post-meta__time">4 mesi fa</div>
             </div>                    
         </div>
     </div>
-    <div class="post__text">Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.</div>
+    <div class="post__text">${post.content}</div>
     <div class="post__image">
-        <img src="https://unsplash.it/600/300?image=171" alt="">
+        <img src="${post.media}" alt="">
     </div>
     <div class="post__footer">
         <div class="likes js-likes">
             <div class="likes__cta">
-                <a class="like-button  js-like-button" href="#" data-postid="1">
+                <a class="like-button  js-like-button" href="#" data-postid="${post.id}">
                     <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
                     <span class="like-button__label">Mi Piace</span>
                 </a>
             </div>
             <div class="likes__counter">
-                Piace a <b id="like-counter-1" class="js-likes-counter">80</b> persone
+                Piace a <b id="like-counter-1" class="js-likes-counter">${post.likes}</b> persone
             </div>
         </div> 
     </div>            
 </div>
 
 </div>`
+console.log(post.author.image);
 })
